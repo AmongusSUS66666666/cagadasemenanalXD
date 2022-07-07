5d3eb378362b6a211226cbca2328c9f6aaf2ee44
AmongusSUS66666666 <108515448+AmongusSUS66666666@users.noreply.github.com>
2022-07-07 02:11:45 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+// creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+    numb: 1,
+    help: "Hiper significa «por encima de lo normal» hipo sinifica «escasez de»",
+    question: "Si la concentración de solutos en el medio externo es igual al interno de una célula se le conoce como:",
+    answer: "Medio isotónico.",
+    options: [
+      "Medio hipertónico.",
+      "Medio hipotónico.",
+      "Medio isotónico.",
+      "Medio hiperosmolar."
+    ]
+  },
+    {
+    numb: 2,
+    help: "Los puentes solo pueden darse entre el elemento que indiquen.",
+    question: "Cuando moléculas NO cargados se encuentran muy cercanos, puede ocurrir una interacción electrostática débil llamada:",
+    answer: "Fuerzas de Van der Waals",
+    options: [
+      "Puentes de hidrógeno",
+      "Enlace Polar",
+      "Puentes disulfuro",
+      "Fuerzas de Van der Waals"
+    ]
+  },
+];
